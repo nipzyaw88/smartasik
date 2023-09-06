3d68fb0b3b1b5936e1c614459a2d37ca4e99e90a
nipz <zaenal@torche.co.id>
2023-09-06 19:01:58 +0700

    fix menu

--- app/components/LeftNav.jsx
+++ app/components/LeftNav.jsx
@@ -8,12 +8,24 @@ const LeftNav = () => {
                     <Link href="/halaman/dashboard" className='link-body-emphasis d-inline-flex text-decoration-none rounded'>Beranda</Link>
                 </li>
                 <li className="mb-1">
-                    <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+                    <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
                         Admisi Rumah Sakit
                     </button>
-                    <div className="collapse show" id="home-collapse">
+                    <div className="collapse" id="home-collapse">
                         <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
                             <li><Link href="/halaman/registrasi-pasien" className='link-body-emphasis d-inline-flex text-decoration-none rounded'>Registrasi Pasien</Link></li>
+                            <li><Link href="/halaman/daftar-pasien-klinik" className='link-body-emphasis d-inline-flex text-decoration-none rounded'>Daftar Pasien Klinik</Link></li>
+                        </ul>
+                    </div>
+                </li>
+                <li className="mb-1">
+                    <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0" data-bs-toggle="collapse" data-bs-target="#home-collapse2" aria-expanded="false">
+                        Layar Publik
+                    </button>
+                    <div className="collapse" id="home-collapse2">
+                        <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                            <li><Link href="/layar-antrian-pendaftaran" target='_blank' className='link-body-emphasis d-inline-flex text-decoration-none rounded'>Layar Antrian Pendaftaran</Link></li>
+                            <li><Link href="/layar-antrian-klinik" target='_blank' className='link-body-emphasis d-inline-flex text-decoration-none rounded'>Layar Antrian Klinik</Link></li>
                         </ul>
                     </div>
                 </li>
